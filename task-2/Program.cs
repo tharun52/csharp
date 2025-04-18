8342434c5c17cf9e009a7fa2c836cf31da24d283
Tharun <thetharun52@gmail.com>
2025-04-18 15:32:59 +0530

    added task2

---
+++ task-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace task2
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, I'm {Name} and I'm {Age} years old.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Alice", 25);
+            Person person2 = new Person("Bob", 30);
+            Person person3 = new Person("Charlie", 22);
+
+            person1.Introduce();
+            person2.Introduce();
+            person3.Introduce();
+        }
+    }
+}
